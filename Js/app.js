9a67eac741d33d98f3b9ee706a1abeb2df89e5b5
Brian <briankagia24@gmail.com>
2025-03-26 23:50:29 +0300

    fixes error in JSON file

--- Js/app.js
+++ Js/app.js
@@ -1,34 +1,35 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    const items = await getItems();
-    const cartItems = await getCartItems()
-    renderItems(items);
+    const tools = await getTools();
+    console.log(tools);
+    renderTools(tools);
+});
 
-})
-
-
-function getTools() {
-    fetch("http://localhost:3000"), {
-        method: "GET"
+async function getTools() {
+    return fetch("http://localhost:3000/tools", {
+        method: "GET",
         headers: {
             Accept: "application/json"
         }
-    }.then((response) => response.json())
+    })
+    .then((response) => response.json())
     .then((data) => data);
 }
 
 function renderTools(tools = []) {
     const toolsContainer = document.querySelector("#tools");
   
-    tools.forEach((tool, index) => {
-      itemsContainer.innerHTML += `
+    toolsContainer.innerHTML = "";
+
+    tools.forEach((tool) => {
+      toolsContainer.innerHTML += `
           <div class="card col-3 my-2 mx-auto" style="width: 18rem;">
-                  <img src="https://placehold.co/400x200" class="card-img-top" alt="...">
+                  <img src="https://placehold.co/400x200" class="card-img-top" alt="Tool Image">
                   <div class="card-body">
-                    <h5 class="card-title">${tool.name}</h5>
-                    <p class="card-text">${tool.price} Ksh</p>
-                    <button id='${tool.id}' class="btn btn-primary btn-sm btn-add">Add To Cart</button>
+                    <h5 class="card-title">${tool.tool_name}</h5>
+                    <p class="card-text">${tool.price_kes} Ksh</p>
+                    <button id='${tool.tool_id}' class="btn btn-primary btn-sm btn-add">Add To Cart</button>
                   </div>
-                </div>
-          `;
+          </div>
+      `;
     });
-  }
+}
